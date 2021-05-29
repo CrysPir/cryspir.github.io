23369c3c557d523efaca59cbdadf605e275f99f8
b.kuznetsov <borivank@yandex.ru>
2021-05-29 14:53:52 +0300

    final push

--- task5/js/main.js
+++ task5/js/main.js
@@ -2,14 +2,30 @@
 	var trigger=0, name, age, sex, education, mathematic, asu, cutting, programming; // обьявляем переменные которые понадобятся в ходе работы
 		document.getElementById("startTest").addEventListener("click",test) // добавляем слушателя события click к элементу с id startTest
 		function test(){ // Функция которая опрашивает пользователя
+			document.getElementById("startTest").style.visibility = 'hidden';
+
 			name = prompt("Введите имя");
-			age = prompt("Введите возраст");
+			while (!(/^([a-zA-Zа-яёА-ЯЁ])/.test(name))) {
+				name = prompt("Введите имя");
+			}
+			age = Number(prompt("Введите возраст"));
+			while (!(/\d/.test(age))) {
+				age = prompt("Введите возраст");
+			}
 			sex = prompt("Введите пол");
+			while ((sex != "М") && (sex != "Ж")) {
+				sex = prompt("Введите пол");
+			}
+			surname = prompt("Введите фамилию");
+			while (!(/^([a-zA-Zа-яёА-ЯЁ])/.test(surname))) {
+				surname = prompt("Введите имя");
+			}
 			education = confirm("У вас есть высше образование?");
 			mathematic = confirm("Вы знаете математику?");
 			asu = confirm("Вы знаете АСУ ТП?");
 			cutting = confirm("Вы знаете теорию резания?");
 			programming = confirm("Умеете программировать?");
+			dancing = confirm("Умеете тацневать?");
 			writeToPage();
 		}
 		function writeToPage(){ // функция которая записывает значение из наших переменных в элементы на странице
@@ -22,14 +38,23 @@
 			$("#name").val(name)
 			$("#age").val(age)
 			$("#sex").val(sex)
+			$("#surname").val(surname)
+			$("#surname").focus();
 			$("#sex").focus();
 			$("#age").focus();
 			$("#name").focus();
 			document.getElementById("education").checked = education;
+			document.getElementById("education").disabled = true;
 			document.getElementById("mathematic").checked = mathematic;
+			document.getElementById("mathematic").disabled = true;
 			document.getElementById("asu").checked = asu;
+			document.getElementById("asu").disabled = true;
 			document.getElementById("cutting").checked = cutting;
+			document.getElementById("cutting").disabled = true;
 			document.getElementById("programming").checked = programming;
+			document.getElementById("programming").disabled = true;
+			document.getElementById("dancing").checked = dancing;
+			document.getElementById("dancing").disabled = true;
 			trigger=1;
 			myFunction();
 		}
@@ -38,7 +63,7 @@
 					//образованием".
 	    	var x = document.getElementById("div1");
 		if (trigger==1) {
-		if (document.getElementById("education").checked ==true && document.getElementById("asu").checked ==true)
+		if (programming == true && mathematic ==true)
 		{
        		var text ='<p class="stroke">ВЫ НАМ ПОДОШЛИ!</p>'
 		x.innerHTML = text;
